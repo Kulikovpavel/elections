d77c2faeaa44ef19acf81d84ce01725732cf7b41
Pavel Kulikov <kulikovpavel@gmail.com>
2014-08-17 00:29:54 +0400

    worker daemon

--- elections_app/models.py
+++ elections_app/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-  name = models.CharField(max_length=200)
+  name = models.CharField(max_length=500)
   birthdate = models.DateField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
-    return self.name 
+    return self.name
 
   class Meta:
     ordering = ('name', 'birthdate', )
 
 class Election(models.Model):
-  name = models.CharField(max_length=300)
+  name = models.CharField(max_length=500)
   date = models.DateField()
   url = models.CharField(max_length=500)
   candidates = models.ManyToManyField(Person, through='Info')
@@ -32,14 +32,14 @@ class Info(models.Model):
   election = models.ForeignKey(Election)
 
   district = models.CharField(max_length=200)
-  party = models.CharField(max_length=300)
+  party = models.CharField(max_length=500)
   address = models.CharField(max_length=1000)
-  edu = models.CharField(max_length=200)
-  firm = models.CharField(max_length=200)
-  job = models.CharField(max_length=200)
-  dep = models.CharField(max_length=200)  # previous deputat status
+  edu = models.CharField(max_length=500)
+  firm = models.CharField(max_length=500)
+  job = models.CharField(max_length=500)
+  dep = models.CharField(max_length=500)  # previous deputat status
   criminal = models.CharField(max_length=1000)
-  status = models.CharField(max_length=200)
+  status = models.CharField(max_length=500)
   url = models.CharField(max_length=500)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
